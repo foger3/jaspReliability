e20052b2cf84c3fc617d5d5819fbc98d7298079e
Luca Thoms <luca.thoms@googlemail.com>
2022-04-14 18:37:39 +0200

    Bland-Altman reference added
    Reference of CI calculations as suggested by Bland & Altman (1986)

--- R/intraclassCorrelation.R
+++ R/intraclassCorrelation.R
@@ -304,6 +304,13 @@ intraclassCorrelation <- function(jaspResults, dataset, options) {
       }
 
       tablesBlandAltman$setData(allData)
+      tablesBlandAltman$addFootnote(
+        gettextf(
+          "%s subjects and 2 measurements. Confidence intervals calculated as referenced by Bland & Altman (1986).",
+          nrow(dataset)
+        )
+      )
+
     }
   }
   return()
